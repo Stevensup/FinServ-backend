0187ee36e18feaa1715756a33c9b206f3aaf68be
Stevenup <114196322+Stevensup@users.noreply.github.com>
2024-10-03 18:59:31 -0500

    Template
    FirstCommit

---
+++ src/main/java/com/edu/unbosque/Digital/FinServ/DigitalFinServApplication.java
@@ -0,0 +1,13 @@
+package com.edu.unbosque.Digital.FinServ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalFinServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalFinServApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/unbosque/Digital/FinServ/DigitalFinServApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.unbosque.Digital.FinServ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalFinServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

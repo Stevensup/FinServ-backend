cf1997fc813ba062418140a2c7e73314fcac4ec0
Stevenup <114196322+Stevensup@users.noreply.github.com>
2024-10-03 19:13:02 -0500

    STEVEB
    CHANGE TO DEPENDENCIES

--- src/main/java/com/edu/unbosque/Digital/FinServ/Controller/CustomerController.java
+++ src/main/java/com/edu/unbosque/Digital/FinServ/Controller/CustomerController.java
@@ -1,5 +1,14 @@
 package com.edu.unbosque.Digital.FinServ.Controller;
 
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Transactional
+@CrossOrigin(origins = { "http://localhost:8090", "http://localhost:8080", "*" })
+@RestController
+@RequestMapping("/customers")
 public class CustomerController {
     
 }

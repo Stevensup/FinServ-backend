89efc865d753f478b553426fc1e066af4726ae83
R3C0NF1G <juliandavidguzman21@gmail.com>
2024-10-07 17:15:06 -0500

    TransactionsModel, Transaction_LogsModel y Transaction_TypesModel

---
+++ src/main/java/com/edu/unbosque/Digital/FinServ/Model/Transaction_LogsModel.java
@@ -0,0 +1,32 @@
+package com.edu.unbosque.Digital.FinServ.Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Date;
+
+@Data
+@Entity
+@Getter
+@Setter
+@Table(name = "transaction_logs")
+public class Transaction_LogsModel {
+
+    @Id
+    @Column(name = "log_id", nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int logId;
+
+    @Column(name = "transaction_id", nullable = false)
+    private int transactionId;
+
+    @Column(name = "log_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date logDate = new Date();
+
+    // Relación con la tabla transactions
+    @ManyToOne
+    @JoinColumn(name = "transaction_id", insertable = false, updatable = false)
+    private TransactionsModel transaction;
+}
---
+++ src/main/java/com/edu/unbosque/Digital/FinServ/Model/TransactionsModel.java
@@ -0,0 +1,48 @@
+package com.edu.unbosque.Digital.FinServ.Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Date;
+
+@Data
+@Entity
+@Getter
+@Setter
+@Table(name = "transactions")
+public class TransactionsModel {
+
+    @Id
+    @Column(name = "transaction_id", nullable = false, updatable = false)
+    private int transactionId;
+
+    @Column(name = "customer_id", nullable = false)
+    private int customerId;
+
+    @Column(name = "product_id", nullable = false)
+    private int productId;
+
+    @Column(name = "transaction_type_id", nullable = false)
+    private int transactionTypeId;
+
+    @Column(name = "amount", nullable = false)
+    private double amount;
+
+    @Column(name = "transaction_date", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date transactionDate;
+
+    // Relaciones con otras tablas (si es necesario agregar)
+    // @ManyToOne
+    // @JoinColumn(name = "customer_id", insertable = false, updatable = false)
+    // private CustomerModel customer;
+
+    // @ManyToOne
+    // @JoinColumn(name = "product_id", insertable = false, updatable = false)
+    // private FinancialProductModel product;
+
+    // @ManyToOne
+    // @JoinColumn(name = "transaction_type_id", insertable = false, updatable = false)
+    // private TransactionTypesModel transactionType;
+}

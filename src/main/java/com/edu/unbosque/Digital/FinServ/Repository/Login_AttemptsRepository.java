b7ce18f198d95eed82211ce0155caa492d745d41
Stevenup <114196322+Stevensup@users.noreply.github.com>
2024-10-07 17:55:08 -0500

    change to camelcase
    camelcase

--- src/main/java/com/edu/unbosque/Digital/FinServ/Repository/Login_AttemptsRepository.java
+++ src/main/java/com/edu/unbosque/Digital/FinServ/Repository/Login_AttemptsRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Login_AttemptsRepository extends JpaRepository<Login_AttemptsModel, Integer>{
+public interface Login_AttemptsRepository extends JpaRepository<Login_AttemptsModel, Integer> {
 }

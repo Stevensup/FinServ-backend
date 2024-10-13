ae5aefdb07fe46ee6163c6d691b21bd4f59f0de3
Stevenup <114196322+Stevensup@users.noreply.github.com>
2024-10-13 16:16:36 -0500

    pom

--- src/main/java/com/edu/unbosque/Digital/FinServ/DigitalFinServApplication.java
+++ src/main/java/com/edu/unbosque/Digital/FinServ/DigitalFinServApplication.java
@@ -3,11 +3,13 @@ package com.edu.unbosque.Digital.FinServ;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application class for DigitalFinServ.
+ */
 @SpringBootApplication
 public class DigitalFinServApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DigitalFinServApplication.class, args);
 	}
-
 }
